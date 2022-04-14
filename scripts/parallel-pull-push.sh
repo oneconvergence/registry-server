32398a522d347a0566f63b35071af4e70399c6d6
Sameer Jathavedan <sameerj137@gmail.com>
2022-04-14 06:05:10 +0000

    Misc changes

--- scripts/parallel-pull-push.sh
+++ scripts/parallel-pull-push.sh
@@ -5,7 +5,7 @@ metrics_file=$2
 log_file=$3 
 
 echo Pulling and pushing the images listed in: $list >> $log_file
-echo Pulling and pushing the images listed in: $list >> $metrics_file
+echo ----------------METRICS: $list -------------- >> $metrics_file
 START=$(date +%s.%N)
 echo START time: $START >> $metrics_file
 echo START size: >> $metrics_file
@@ -19,3 +19,4 @@ DIFF=$(echo "$END - $START" | bc)
 echo DIFF time: $DIFF >>  $metrics_file
 echo Done pulling and pushing images from: $list >> $log_file
 echo Done pulling and pushing images from: $list >> $metrics_file
+echo -------------------------------------------------------- >> $metrics_file
